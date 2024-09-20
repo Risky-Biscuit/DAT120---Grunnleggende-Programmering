5344a6896d3299b8f3514b24b49ab00993693181
Kristian Gundersen <kissa.gundersen@gmail.com>
2024-09-20 20:58:30 +0200

    Slettet rainbow_benzene da den ble flyttet til Sandbox prosjektet i stedet. Legger til frivillige Oppgave_5b.py.

--- Øving 5/Oppgave_5b.py
+++ Øving 5/Oppgave_5b.py
@@ -3,4 +3,44 @@ Frivillig, avansert: Endre programmet slik at annenhver diamant er fylt med svar
 fylt med hvit. Start med å fylle med svart. Under er det et eksempel med 5 diamanter. Du kan også
 prøve deg på mer enn to farger.
 """
+
 import turtle
+
+# Ber bruker om input for antall diamanter som skal tegnes
+antall_diamanter = int(input("Hvor mange diamanter skal vi tegne? "))
+
+# Setter en størrelse til å starte med og et increment jeg ønsker å bruke som utgangspunkt til å øke størrelsen
+# på diamantene
+base_size = 680
+increment = 70
+
+# Lager en definisjon som tegner diamantene
+def draw_diamonds(current_size, colour):
+    turtle.fillcolor(colour)
+    turtle.begin_fill()
+    turtle.pendown()
+    for x in range(4):
+        turtle.forward(current_size)
+        turtle.left(90)
+    turtle.end_fill()
+
+# Tar turtle til startposisjon
+turtle.penup()
+turtle.goto(antall_diamanter*50 + 50, 0)
+turtle.left(180)
+
+# Setter opp en for loop for å tegne diamantene
+for y in range(antall_diamanter):
+    current_size = base_size - y*increment
+    if y % 2 == 0:
+        colour = "black"
+    else:
+        colour = "white"
+    turtle.forward(50)
+    turtle.right(45)
+    turtle.pendown()
+    draw_diamonds(current_size, colour)
+    turtle.left(45)
+    turtle.penup()
+
+turtle.done()
