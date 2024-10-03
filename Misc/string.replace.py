e963e94ff2a4d2c256e2e839e805c7b944ef7d66
Kristian Gundersen <kissa.gundersen@gmail.com>
2024-10-03 14:49:06 +0200

    La til en fil som demonstrerer .replace

---
+++ Misc/string.replace.py
@@ -0,0 +1,15 @@
+"""
+Viser hvordan man bruker .replace kommandoen
+"""
+
+# Lager en string
+string = "Dette er en test"
+
+# Printer stringen
+print(string)
+
+# Erstatter "Dette" med "Denne"
+new_string = string.replace("Dette", "Denne")
+
+# Skriver ut den nye stringen
+print(new_string)
